51096d2e1e42cb85c5ae00b09d3a6e7ec89d66d4
MakiKuri00 <hpham04.uw.edu>
2026-06-25 13:25:25 -0700

    Arduino code for sensors

---
+++ robot arm/sensor/sensor.ino
@@ -0,0 +1,57 @@
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+const int C = 2;
+int TRIG_PIN = 12;
+int ECHO_PIN = 11;
+
+int THERMO_SCL = 21;
+int THERMO_SDA = 22;
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  if (!mlx.begin()) {
+    Serial.println("Failed to find MLX90614!");
+    while (1); 
+  }
+  Serial.println("ultrasonic, temp");
+}
+
+float getDistanceM() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH);
+
+  float distanceM = (duration * 0.0343) / 200.0;
+
+  return distanceM;
+}
+
+void loop() {
+  float d = getDistanceM(); // Object's distance from sensor
+  Serial.print(d);
+  Serial.print(", ");
+  if (d <= 3.00) {
+    // measure temp
+    double currentTemp = mlx.readObjectTempC();
+    Serial.print(currentTemp);
+  } else {
+    // idle
+    Serial.print("___");
+  }
+
+  Serial.println();
+  delay(100);
+  
+}
